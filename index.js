920ed3f3ed641ece361f657913688be45d8c689d
Moises Galdino <90233062+moisesgaldino@users.noreply.github.com>
2023-12-27 15:44:16 -0300

    refactor:added the program architecture

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
+const userRoute = require("./src/routes/user.router");
 const app = express();
 
+app.use("/soma", userRoute);
+
 //ROTA
-  //METHOD HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-    //GET - Pega uma info
-    //POST - Cria uma info
-    //PUT - Altera toda info
-    //PATH - Altera parte da info
-    //DELETE - Apaga uma info
+//METHOD HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+//GET - Pega uma info
+//POST - Cria uma info
+//PUT - Altera toda info
+//PATH - Altera parte da info
+//DELETE - Apaga uma info
 
-  //NAME - Um identificador da rota
-    //
-  //Function (callback) - Responsavel por executar algum comando
+//NAME - Um identificador da rota
+//
+//Function (callback) - Responsavel por executar algum comando
 
-app.get('/', (req, res)  => {
+/*app.get("/soma", (req, res)  => {
   const soma = 100 + 1;
 
   res.send({soma: soma})
-});
+});*/
 
 app.listen(3000);
