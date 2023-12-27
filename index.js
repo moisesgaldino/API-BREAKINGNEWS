7fa3ef918c00cad60e1b5c0f835fb39e1ce988d5
Moises Galdino <90233062+moisesgaldino@users.noreply.github.com>
2023-12-27 15:21:46 -0300

    refactor:add rout soma

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+//ROTA
+  //METHOD HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+    //GET - Pega uma info
+    //POST - Cria uma info
+    //PUT - Altera toda info
+    //PATH - Altera parte da info
+    //DELETE - Apaga uma info
+
+  //NAME - Um identificador da rota
+    //
+  //Function (callback) - Responsavel por executar algum comando
+
 app.get('/', (req, res)  => {
-  res.send('Hello World');
+  const soma = 100 + 1;
+
+  res.send({soma: soma})
 });
 
 app.listen(3000);
